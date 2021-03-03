d773ae3b0b872123a63e1378fa0c161728198ec2
peka80 <petar.dragovic@gmail.com>
2021-03-03 14:58:54 +0100

    var, let & const

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,1 +1,50 @@
-// Log to console
+// var, let, const
+
+// var name = "John Doe";
+// console.log(name);
+
+// name = "Steve Smith";
+// console.log(name);
+
+// Init var
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+// letters, numbers, _, $
+// Can not start with number
+
+// Multi word variables
+var firstName = "Name"; // Camel case
+var first_name = "Sara"; // Underscore
+var FirstName = "Tom"; // Pascal case
+
+// LET
+// let greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+// CONST
+// const ime = "John";
+// console.log(ime);
+// Can not reassign
+// ime = "Sara";
+// Have to assign value
+// const greeting;
+
+const person = {
+  name: "John",
+  age: 30,
+};
+
+person.name = "Sara";
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers);
